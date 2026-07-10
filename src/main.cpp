90fb57991a409c0b89a83ca3404905709696b966
Carmen Moreno <carmen.moreno.udaondo@gmail.com>
2026-07-10 13:56:27 +0200

    Refactor WiFi connection handling in setup and loop functions

--- src/main.cpp
+++ src/main.cpp
@@ -25,24 +25,27 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
 void setup() {
     Serial.begin(115200);
+    delay(1000);
 
     Wire.begin(SCREEN.sda_pin, SCREEN.scl_pin);
     Wire.setClock(400000);
 
     u8g2.begin();
 
+    delay(500);
     WiFi.begin(WIFI_CONFIG.ssid, WIFI_CONFIG.password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("WiFi connected");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
 }
 
 void loop() {
-
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.print("Connecting to WiFi...");
+        delay(1000);
+    } else {
+        Serial.println("Connected to WiFi");
+        u8g2.clearBuffer();
+        u8g2.setFont(u8g2_font_ncenB08_tr);
+        u8g2.drawStr(0, 10, "Connected to WiFi");
+        u8g2.sendBuffer();
+        delay(5000);
+    }
 }
