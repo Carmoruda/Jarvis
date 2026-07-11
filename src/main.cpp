5289a39d4ec030309b91325bc8e11d0d483e9c63
Carmen Moreno <carmen.moreno.udaondo@gmail.com>
2026-07-11 22:17:50 +0200

    Refactor WiFi connection handling and integrate NTP server configuration

--- src/main.cpp
+++ src/main.cpp
@@ -20,48 +20,74 @@ struct WiFiConfig {
     public:
         const char *ssid;
         const char *password;
+        char *ntpServer;
 };
 
 constexpr Screen SCREEN = {21, 22};
-constexpr WiFiConfig WIFI_CONFIG = {WIFI_SSID, WIFI_PASS};
-Button BUTTON = {32, false};
+constexpr WiFiConfig WIFI_CONFIG = {WIFI_SSID, WIFI_PASS, "pool.ntp.org"};
 
-const char* ntpServer = "pool.ntp.org";
 const char *time_zone = "CET-1CEST,M3.5.0,M10.5.0/3"; // Central European Time (CET) with daylight saving time
-
 
 // LED Screen config (Rotation, Reset pin)
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
+void connectWiFi()
+{
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.drawStr((128 - u8g2.getStrWidth("Connecting to WiFi...")) / 2, 30, "Connecting to WiFi...");
+    u8g2.sendBuffer();
+
+    WiFi.begin(WIFI_CONFIG.ssid, WIFI_CONFIG.password);
+
+    uint8_t retry = 0;
+
+    while (WiFi.status() != WL_CONNECTED && retry < 100) {
+        delay(300);
+        retry++;
+    }
+
+    if (WiFi.status() == WL_CONNECTED) {
+
+        u8g2.clearBuffer();
+        u8g2.setFont(u8g2_font_ncenB08_tr);
+        u8g2.drawStr((128 - u8g2.getStrWidth("WiFi connected!")) / 2, 30, "WiFi connected!");
+        u8g2.sendBuffer();
+
+        delay(500);
+
+    } else {
+        u8g2.clearBuffer();
+        u8g2.setFont(u8g2_font_ncenB08_tr);
+        u8g2.drawStr((128 - u8g2.getStrWidth("WiFi FAILED!")) / 2, 30, "WiFi FAILED!");
+        u8g2.sendBuffer();
+
+        delay(1500);
+    }
+}
+
+void syncTime()
+{
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.drawStr((128 - u8g2.getStrWidth("Syncing time...")) / 2, 30, "Syncing time...");
+    u8g2.sendBuffer();
+
+    configTzTime(time_zone, WIFI_CONFIG.ntpServer);
+
+    delay(1000);
+}
+
 void setup() {
     Serial.begin(115200);
-    delay(1000);
 
     Wire.begin(SCREEN.sda_pin, SCREEN.scl_pin);
     Wire.setClock(400000);
 
     u8g2.begin();
 
-    delay(500);
-    WiFi.begin(WIFI_CONFIG.ssid, WIFI_CONFIG.password);
-
-    while(WiFi.status() != WL_CONNECTED) {
-        u8g2.clearBuffer();
-        u8g2.setFont(u8g2_font_ncenB08_tr);
-        u8g2.drawStr(0, 10, "Waiting for WiFi...");
-        u8g2.sendBuffer();
-
-        delay(250);
-    }
-
-    u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.drawStr(0, 10, "Connected to WiFi");
-    u8g2.sendBuffer();
-
-    configTzTime(time_zone, ntpServer);
-
-    pinMode(BUTTON.pin, INPUT_PULLUP);
+    connectWiFi();
+    syncTime();
 
     delay(100);
 }
@@ -93,14 +119,4 @@ void loop() {
     u8g2.drawStr(0, 60, "jarvis v0.1");
 
     u8g2.sendBuffer();
-
-    int lectura = digitalRead(BUTTON.pin);
-
-    if (lectura != BUTTON.state) {
-        BUTTON.state = lectura;
-
-        if (lectura == LOW) Serial.println("pulsado");
-        else                Serial.println("soltado");
-    }
-
 }
