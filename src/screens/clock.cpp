d5eedc48e999cdac944beec8fe45bc49d0583c10
Carmen Moreno <carmen.moreno.udaondo@gmail.com>
2026-07-12 21:02:18 +0200

    Add clock module with time display and setup functions in clock.cpp and clock.h

---
+++ src/screens/clock.cpp
@@ -0,0 +1,41 @@
+#include "clock.h"
+#include <Arduino.h>
+#include "hardware/display.h"
+
+// Time zone configuration
+const char *time_zone = "CET-1CEST,M3.5.0,M10.5.0/3"; // Central European Time (CET) with daylight saving time
+static int prevHour = -1;
+static int prevMin = -1;
+
+void drawClock(String hourStr, String minStr) {
+
+    // Clear the buffer and set the font
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_freedoomr25_tn);
+
+    String timeStr = hourStr + ":" + minStr;
+
+    // Draw the time in the center of the screen
+    u8g2.setCursor((128 - u8g2.getStrWidth(timeStr.c_str())) / 2, 50);
+    u8g2.print(hourStr);
+    u8g2.print(":");
+    u8g2.print(minStr);
+}
+
+void clockSetup()
+{
+    // Get the current time
+    struct tm time_info;
+    getLocalTime(&time_info);
+
+    char hour[3], min[3];   // 2 digits + null terminator
+    snprintf(hour, sizeof(hour), "%02d", time_info.tm_hour);
+    snprintf(min, sizeof(min), "%02d", time_info.tm_min);
+
+    // Only update the display if the time has changed
+    if (time_info.tm_hour != prevHour || time_info.tm_min != prevMin) {
+        drawClock(String(hour), String(min));
+        prevHour = time_info.tm_hour;
+        prevMin = time_info.tm_min;
+    }
+}
---
+++ src/screens/clock.h
@@ -0,0 +1,6 @@
+#pragma once
+
+extern const char *time_zone;
+
+void clockSetup();
+void drawClock();
